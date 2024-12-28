b1e0ced57eea7c26bf5b5c8e09f1d1c2861b0984
Ryan <aa0900144903@gmail.com>
2024-12-29 03:45:55 +0800

    feat(api-type): add IGetApiSuccessBaseWithAmount

--- src/types/api.type.ts
+++ src/types/api.type.ts
@@ -5,3 +5,7 @@ export interface IApiRes {
 export interface IGetApiSuccessBase<T> extends IApiRes {
   data: T;
 }
+export interface IGetApiSuccessBaseWithAmount<T> extends IApiRes {
+  data: T;
+  total: number;
+}
